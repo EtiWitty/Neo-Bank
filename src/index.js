1c3ee951c2510ecf1aa9cafbc628b6015828500b
EtiWitty <atiyeh.keshavarz@gmail.com>
2019-10-19 16:16:53 -0700

    Added SignInSide template from material-ui

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/styles';
 import App from './App';
+import SignInSide from "./SignInSide";
 import theme from './theme';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
-    <App />
+    {/* <App /> */}
+    <SignInSide />
   </ThemeProvider>,
   document.querySelector('#root'),
 );
