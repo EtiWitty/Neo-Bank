9c88c5131609a0ec2ec85a86843dada11a90bf40
EtiWitty <atiyeh.keshavarz@gmail.com>
2019-10-21 14:31:42 -0700

    adjusted small changes

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/styles';
-import App from './App';
+//import App from './App';
 import SignInSide from "./SignInSide";
 import theme from './theme';
 
