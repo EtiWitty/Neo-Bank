cae0ceab7d0c1981ff7f0c2e694da67677439186
EtiWitty <atiyeh.keshavarz@gmail.com>
2019-10-22 14:21:56 -0700

    setup store

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,16 @@ import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
 import ProTip from './ProTip';
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
 
 function Copyright() {
   return (
@@ -19,6 +27,23 @@ function Copyright() {
       {'.'}
     </Typography>
   );
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default function App() {
