23581ae3b0ba78fa317669b24e1537bbba5df4ac
EtiWitty <atiyeh.keshavarz@gmail.com>
2019-10-22 12:52:05 -0700

    moved files

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: "mongodb+srv://etiwitty2:HackReactor2019@fintech-4kgjy.mongodb.net/Synapse?retryWrites=true&w=majority",
+//  mongoURI: "mongodb+srv://etiwitty2:HackReactor2019@fintech-4kgjy.mongodb.net/Synapse",
+  secretOrKey: "secret"
+};
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 
 const users = require("./routes/api/users");
-const plaid = require("./routes/api/plaid");
+//const plaid = require("./server/routes/api/plaid");
 
 const app = express();
 
@@ -41,7 +41,7 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
-app.use("/api/plaid", plaid);
+// app.use("/api/plaid", plaid);
 
 const port = process.env.PORT || 5000;
 
