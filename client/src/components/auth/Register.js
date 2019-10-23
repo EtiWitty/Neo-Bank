fadbc4bab2648962199ef0b0fcb77b1aa75b2fd4
EtiWitty <atiyeh.keshavarz@gmail.com>
2019-10-23 13:14:25 -0700

    redirect users to the dashboard if they navigate to Register page

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -16,6 +16,14 @@ class Register extends Component {
       errors: {}
     };
   }
+
+  componentDidMount() {
+    // If logged in and user navigates to Register page, should redirect them to dashboard
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+  
 componentWillReceiveProps(nextProps) {
     if (nextProps.errors) {
       this.setState({
