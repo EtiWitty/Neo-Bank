7bce5828fbd417033bbfc708679181689869f9ff
EtiWitty <atiyeh.keshavarz@gmail.com>
2019-10-25 20:22:09 -0700

    removing keys.js

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  // mongoURI: "mongodb+srv://etiwitty2:HackReactor2019@fintech-4kgjy.mongodb.net/Synapse?retryWrites=true&w=majority",
-  // on heroku
-  mongoURI: "mongodb://etiwitty2:HackReactor2019@ds139768.mlab.com:39768/heroku_5wnv1vrf",
-  secretOrKey: "secret"
-};
